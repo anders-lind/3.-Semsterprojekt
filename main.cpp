6db1161caf09220817f0351bbef3d1a7c98cb0a9
Anderslind <32484749+anders-lind@users.noreply.github.com>
2021-10-09 19:57:14 +0200

    CMakeLists, Main file and throw class

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include<cmath>
+
+#include "throw.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    std::vector<double> start {10,0,0};
+    std::vector<double> target {20,0,0};
+
+    Throw throw1(start, target);
+    std::vector<double> distance = throw1.getDistance();
+    double speed = throw1.getSpeed();
+
+    std::cout << "Speed = " << speed << std::endl;
+
+
+
+
+    return 0;
+}
