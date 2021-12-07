d4132d32847faed00062809fa295453975371719
Anderslind <32484749+anders-lind@users.noreply.github.com>
2021-12-07 12:18:00 +0100

    Test

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,7 +1,7 @@
 #include "robot.h"
-#include "kinematics.h"
-#include "jacobian.h"
-#include "chrono"
+
+using namespace std;
+
 
 robot::robot()
 {
@@ -42,7 +42,7 @@ void robot::startingPosition()
     bool async = false;
     //Moves the robot
     m_control->moveJ(coordinates, speed, acc, async);
-}
+    }
 
 void robot::pickUpBall(std::vector<double> coordinates)
 {
@@ -62,15 +62,15 @@ void robot::goToThrowPos(){
 }
 
 void robot::throwBall(std::vector<double> goalCoordinates, double t)
-{
+{   
     // Joint values for throw position
     std::vector<double> q_kV = {99, -90, 106, -124, -85, -101};
     radConversion(q_kV);
-
+    
     // Cartisian coordinates for throw position
     std::vector<double> x_kV = {0.4674, -0.1943, -0.043, 0, 0, 0};
     Eigen::MatrixXd x_mE = jacobian::vec2Eig(goalCoordinates);
-
+    
 
     Kinematics kin;
 
@@ -93,7 +93,7 @@ void robot::throwBall(std::vector<double> goalCoordinates, double t)
     for(int i = 0; i < jacobian::eig2Vec(qp_k).size(); ++i){
         qp_k_Neg.at(i) = jacobian::eig2Vec(qp_k).at(i) * (-1);
     }
-
+    
     // Go to x_s
     m_control->speedJ(qp_k_Neg, max_acc, t);
     std::this_thread::sleep_for(std::chrono::duration<double>(t)); // Skal msåke kun bruges når man bruger den rigtige robot
@@ -104,33 +104,7 @@ void robot::throwBall(std::vector<double> goalCoordinates, double t)
     std::this_thread::sleep_for(std::chrono::duration<double>(t)); // Skal msåke kun bruges når man bruger den rigtige robot
     m_control->speedStop();
 }
-/*
-void robot::throwBall(std::vector<double> goalCoordinates)
-{
-    std::vector<double> q_k = {99, -90, 106, -124, -85, -101};
-    radConversion(q_k);
-    std::vector<double> x_k = {0.4674, -0.1943, -0.043, 0, 0, 0};
-    Eigen::MatrixXd x_m = jacobian::vec2Eig(goalCoordinates);
-    Kinematics kin;
-    Eigen::MatrixXd xp_k = kin.calc_xp_k(jacobian::vec2Eig(x_k), x_m);
-    Eigen::MatrixXd qp_k = kin.calc_qp_k(jacobian::vec2Eig(q_k), xp_k);
-    Eigen::MatrixXd acc = kin.calc_acc(qp_k, 0.5);
-    double max_acc = kin.calc_max_acc(acc);
 
-    std::vector<double> test(6);
-    for(int i = 0; i < jacobian::eig2Vec(qp_k).size(); ++i){
-        test.at(i) = jacobian::eig2Vec(qp_k).at(i) * (-1);
-    }
-
-    m_control->speedJ(test, max_acc, 0.5);
-    sleep(double(0.5));
-    m_control->speedStop();
-
-    m_control->speedJ(jacobian::eig2Vec(qp_k), max_acc, 0.5);
-    sleep(double(0.5));
-    m_control->speedStop();
-}
-*/
 void robot::radConversion(std::vector<double> &jPose)
 {
     //Degrees to rad conversion
